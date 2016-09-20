211e7baeb1931dec91c8687ab55b1e0eba714d26
赵吉彤 <zjtong3576@sina.com>
2016-09-20 20:31:05 +0800

    myadd

--- Robot/main.js
+++ Robot/main.js
@@ -22,6 +22,7 @@ var getText = function (inner) {
         success: function (data) {
             // console.log(data);
             console.log(data.result.text);
+            getAK(data.result.text);
         },
         dataType: 'JSONP'
     });
@@ -32,4 +33,48 @@ var sayOut = function () {
     var theText = $("#diyKeyword").val();
     console.log(theText);
     getText(theText);
+}
+
+var getAK = function (mainStr) {
+    // alert(mainStr);
+
+    $.ajax({
+        type: 'POST',
+        dataType: 'JSONP',
+        url: 'https://openapi.baidu.com/oauth/2.0/token',
+        data: {
+            grant_type: 'client_credentials',
+            client_id: '9HO189xz4FAPCoji5fPe8yhG',
+            client_secret: 'e109162a8056190d6a07662637fbd64a'
+        },
+        success: function (data) {
+            console.log(data);
+            readIt( mainStr , '24.f66bbeeef7a98404f5d21cbd7739d15d.2592000.1476965633.282335-8650753');
+        },
+        // error: function () {
+        //     alert("Error!");
+        // }
+    });
+
+    readIt( mainStr , '24.f66bbeeef7a98404f5d21cbd7739d15d.2592000.1476965633.282335-8650753');
+}
+
+
+var readIt = function ( text , ak ) {
+    $.ajax({
+        type: 'POST',
+        dataType: 'jsonp',
+        url: 'http://tsn.baidu.com/text2audio',
+        data: {
+            tex: text,
+            lan: 'zh',
+            tok: 'ak',
+            ctp: 1,
+            cuid: '123423534'
+        },
+        success: function (data) { },
+        error: function () {
+            alert("Error!");
+        }
+    });
 }
